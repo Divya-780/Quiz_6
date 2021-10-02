db9d78663891f9dcbf67381393d2abeb9645a2fe
Divya-780 <90021716+Divya-780@users.noreply.github.com>
2021-10-02 11:37:49 +0530

    Update frequency _of_words.py

--- frequency _of_words.py
+++ frequency _of_words.py
@@ -7,8 +7,5 @@ for i in words:
         dict_[i]=dict_[i]+1
     else:
         dict_[i]=1
-#print(dict)
-a = sorted(dict_.values(),reverse=True)
-print(a)
-#for keys,a in dict_.items():
- # print(keys,"=",a)
+
+print(sorted(dict_.items()))
